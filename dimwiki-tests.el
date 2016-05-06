3deb4c3c0d4887c851ad54cdf92d8ed172d912c2
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-05-06 19:04:53 +0200

    tests into own file, -- for internal fn's, cleanups

---
+++ dimwiki-tests.el
@@ -0,0 +1,65 @@
+;;; dimwiki-tests.el --- tests for dimwiki.el
+
+;; Copyright (C) 2014-2016  Kevin Brubeck Unhammer <unhammer@fsfe.org>
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Tests
+
+;;; Code:
+
+(require 'ert)
+(require 'dimwiki)
+
+(ert-deftest dimwiki--parse ()
+  :tags '(dimwiki)
+  (should (equal (dimwiki--parse "foo {{fie blah}} bar")
+		 '(doc "foo "
+		       (tmpl "fie blah")
+		       " bar")))
+  (should (equal (dimwiki--parse "foo [[fie blah]] bar")
+		 '(doc "foo "
+		       (link "fie blah")
+		       " bar")))
+  (should (equal (dimwiki--parse "foo {{fie[[with ]]blah}} bar")
+		 '(doc "foo "
+		       (tmpl "fie" (link "with ") "blah")
+		       " bar")))
+  (should (equal (dimwiki--parse "foo '''meh''' bar")
+		 '(doc "foo "
+		       "'''"		; too stupid to parse these :-)
+		       "meh"
+		       "'''"
+		       " bar")))
+  (should (equal (dimwiki--parse "foo [[ {{fie [[fum foo]] blah}} ]] bar")
+		 '(doc "foo "
+		       (link " "
+			     (tmpl "fie "
+				   (link "fum foo")
+				   " blah")
+			     " ")
+		       " bar")))
+  (should (equal (dimwiki--parse "foo [[fie|blah]] bar")
+		 '(doc "foo "
+		       (link (ref "fie") "blah")
+		       " bar"))))
+
+(defun dimwiki--run-tests ()
+  (ert-run-tests-interactively 'dimwiki--parse))
+
+
+(provide 'dimwiki-tests)
+;;; dimwiki-tests.el ends here
--- dimwiki.el
+++ dimwiki.el
@@ -1,9 +1,11 @@
 ;;; dimwiki.el --- a dim-witted wiki parser and lookup tool
 
-;; Copyright (C) 2014  Kevin Brubeck Unhammer <unhammer@fsfe.org>
+;; Copyright (C) 2014-2016  Kevin Brubeck Unhammer <unhammer@fsfe.org>
 
 ;; Author: Kevin Brubeck Unhammer <unhammer@fsfe.org>
+;; Version: 0.1.0
 ;; Keywords: help, hypermedia
+;; Package-Requires: ()
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
@@ -28,7 +30,6 @@
 
 (require 'json)
 (require 'url)
-(require 'ert)
 
 (defvar dimwiki-wikidomains '("nn.wikipedia.org"
 			      "no.wikipedia.org"
@@ -52,69 +53,69 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
 
 ;;; Tokeniser:
 
-(defconst dimwiki-nontext
+(defconst dimwiki--nontext
   '( ?\[
      ?\]
      ?{
      ?}
      ?'
      ?| ))
-(defconst dimwiki-nontext-re
-  (regexp-opt (mapcar 'char-to-string dimwiki-nontext)))
+(defconst dimwiki--nontext-re
+  (regexp-opt (mapcar 'char-to-string dimwiki--nontext)))
 
-(defun dimwiki-eat (from)
+(defun dimwiki--eat (from)
   (let ((char (char-after from)))
-    (if (memq char dimwiki-nontext)
-	(dimwiki-eat-char char)
-      (dimwiki-eat-text))))
+    (if (memq char dimwiki--nontext)
+	(dimwiki--eat-char char)
+      (dimwiki--eat-text))))
 
-(defun dimwiki-eat-text ()
-  (when (re-search-forward dimwiki-nontext-re (point-max) 'noerror)
+(defun dimwiki--eat-text ()
+  (when (re-search-forward dimwiki--nontext-re (point-max) 'noerror)
     (goto-char (- (match-end 0) 1))))
 
-(defun dimwiki-eat-char (char)
+(defun dimwiki--eat-char (char)
   (while (and (eq (char-after (point)) char)
 	      (< (point) (point-max)))
     (forward-char)))
 
-(defun dimwiki-tokenise-buffer ()
+(defun dimwiki--tokenise-buffer ()
   (let ((i 0)
 	(from (point))
 	tokens)
     (while (and (< (point) (point-max))
 		(< i dimwiki-max-tokens))
       (setq i (+ 1 i))
-      (dimwiki-eat from)
+      (dimwiki--eat from)
       (setq tokens (cons (buffer-substring-no-properties from (point)) tokens)
 	    from (point)))
     (reverse tokens)))
 
-(defun dimwiki-tokenise-string (string)
+(defun dimwiki--tokenise-string (string)
   (with-temp-buffer
     (insert string)
     (goto-char (point-min))
-    (dimwiki-tokenise-buffer)))
+    (dimwiki--tokenise-buffer)))
 
 
 ;;; Parser:
-(defvar dimwiki-tokens '()
+(defvar dimwiki--tokens '()
   "Modified imperatively, but with dynamic scope.")
-(defun dimwiki-gettok ()
-  (and dimwiki-tokens (pop dimwiki-tokens)))
-(defun dimwiki-peektok ()
-  (and dimwiki-tokens (car dimwiki-tokens)))
+(defun dimwiki--gettok ()
+  (and dimwiki--tokens (pop dimwiki--tokens)))
+(defun dimwiki--peektok ()
+  (and dimwiki--tokens (car dimwiki--tokens)))
 
-(defun dimwiki-parse (string)
-  (let ((dimwiki-tokens (dimwiki-tokenise-string string)))
-    (cons 'doc (dimwiki-doc))))
+(defun dimwiki--parse (string)
+  (let ((dimwiki--tokens (dimwiki--tokenise-string string)))
+    (cons 'doc (dimwiki--doc))))
 
-(defun dimwiki-doc ()
+(defun dimwiki--doc ()
   (let (parsed cur-tok)
     (catch 'break
-      (while (setq cur-tok (dimwiki-peektok))
+      (while (setq cur-tok (dimwiki--peektok))
 	(cond ((equal "[[" cur-tok)
-	       (dimwiki-gettok)
-	       (setq parsed (cons (dimwiki-link) parsed)))
+	       (dimwiki--gettok)
+	       (setq parsed (cons (dimwiki--link) parsed)))
 	      ;; TODO: since we simply return nil here, -tmpl has to
 	      ;; handle ]] and -link handle }} to avoid stopping the parse
 	      ;; process -- not good.
@@ -123,12 +124,12 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
 	      ((equal "|" cur-tok)
 	       (throw 'break nil))
 	      ((equal "{{" cur-tok)
-	       (dimwiki-gettok)
-	       (setq parsed (cons (dimwiki-tmpl) parsed)))
+	       (dimwiki--gettok)
+	       (setq parsed (cons (dimwiki--tmpl) parsed)))
 	      ((equal "}}" cur-tok)
 	       (throw 'break nil))
 	      (cur-tok
-	       (dimwiki-gettok)
+	       (dimwiki--gettok)
 	       (setq parsed (cons cur-tok parsed)))
 	      (t (throw 'break nil)))))
     (reverse parsed)))
@@ -136,13 +137,13 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
 
 ;;; TODO: external/single-bracket [http://foo links]
 
-(defun dimwiki-link ()		; we've seen a [[
-  (let ((parsed (dimwiki-doc))
-	(cur-tok (dimwiki-gettok)))
+(defun dimwiki--link ()		; we've seen a [[
+  (let ((parsed (dimwiki--doc))
+	(cur-tok (dimwiki--gettok)))
     (cond ((equal "]]" cur-tok)
 	   (cons 'link parsed))
 	  ((equal "|" cur-tok)
-	   (let* ((parsed-end (dimwiki-link))
+	   (let* ((parsed-end (dimwiki--link))
 		  (tag (car parsed-end))
 		  (text (cdr parsed-end))
 		  (ref (cons 'ref parsed)))
@@ -150,54 +151,19 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
 	     (cons tag (cons ref text))))
 	  (t (cons 'link? parsed)))))
 
-(defun dimwiki-tmpl ()		; we've seen a {{
-  (let ((parsed (dimwiki-doc))
-	(cur-tok (dimwiki-gettok)))
+(defun dimwiki--tmpl ()		; we've seen a {{
+  (let ((parsed (dimwiki--doc))
+	(cur-tok (dimwiki--gettok)))
     (cond ((equal "}}" cur-tok)
 	   (cons 'tmpl parsed))
 	  ((equal "|" cur-tok)
-	   (let* ((parsed-end (dimwiki-tmpl))
+	   (let* ((parsed-end (dimwiki--tmpl))
 		  (tag (car parsed-end))
 		  (text (cdr parsed-end))
 		  (ref (cons 'ref parsed)))
 	     (cons tag (cons ref text))))
 	  (t (cons 'tmpl? parsed)))))
 
-(ert-deftest dimwiki-parse ()
-  :tags '(dimwiki)
-  (should (equal (dimwiki-parse "foo {{fie blah}} bar")
-		 '(doc "foo "
-		       (tmpl "fie blah")
-		       " bar")))
-  (should (equal (dimwiki-parse "foo [[fie blah]] bar")
-		 '(doc "foo "
-		       (link "fie blah")
-		       " bar")))
-  (should (equal (dimwiki-parse "foo {{fie[[with ]]blah}} bar")
-		 '(doc "foo "
-		       (tmpl "fie" (link "with ") "blah")
-		       " bar")))
-  (should (equal (dimwiki-parse "foo '''meh''' bar")
-		 '(doc "foo "
-		       "'''"		; too stupid to parse these :-)
-		       "meh"
-		       "'''"
-		       " bar")))
-  (should (equal (dimwiki-parse "foo [[ {{fie [[fum foo]] blah}} ]] bar")
-		 '(doc "foo "
-		       (link " "
-			     (tmpl "fie "
-				   (link "fum foo")
-				   " blah")
-			     " ")
-		       " bar")))
-  (should (equal (dimwiki-parse "foo [[fie|blah]] bar")
-		 '(doc "foo "
-		       (link (ref "fie") "blah")
-		       " bar"))))
-
-(defun dimwiki-run-tests ()
-  (ert-run-tests-interactively 'dimwiki-parse))
 
 
 
@@ -206,7 +172,7 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
 
 ;;; Network, json stuff:
 
-(defun dimwiki-delete-http-header ()
+(defun dimwiki--delete-http-header ()
   "Delete the HTTP header in the current buffer."
   (goto-char (point-min))
   (let ((beg (point-min))
@@ -214,7 +180,7 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
     (when end
       (delete-region beg end))))
 
-(defun dimwiki-send-search-query (term domain)
+(defun dimwiki--send-search-query (term domain)
   (let ((url-request-method "GET")
 	(url-request-data "")
 	(url-request-extra-headers
@@ -233,12 +199,12 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
 			  (format "https://%s/w/api.php?%s" domain get-data))
       (unwind-protect
 	  (let ((results ""))
-	    (dimwiki-delete-http-header)
+	    (dimwiki--delete-http-header)
 	    (setq results (buffer-string))
 	    results)
 	(kill-buffer)))))
 
-(defun dimwiki-extract-article-from-json (json-string)
+(defun dimwiki--extract-article-from-json (json-string)
   (let ((json-object-type 'plist)
 	(json-array-type 'list))
     (plist-get
@@ -253,7 +219,7 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
        :revisions))
      :*)))
 
-(defun dimwiki-skiplink (elt)
+(defun dimwiki--skiplink (elt)
   (or (and (stringp (car elt))
 	   (string-match "^Kategori" (car elt)))
       (and (listp (car elt))
@@ -261,37 +227,37 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
 	   (string-match "\\.jpg" (car (cdar elt))))))
 
 
-;;; TODO: less recursive, fails on long texts (thus dimwiki-max-tokens)
-(defun dimwiki-justext (parsed)
+;;; TODO: less recursive, fails on long texts (thus dimwiki--max-tokens)
+(defun dimwiki--justext (parsed)
   (let ((head (car parsed))
 	(tail (cdr parsed)))
     (cond ((eq head 'doc)
-	   (dimwiki-justext tail))
+	   (dimwiki--justext tail))
 	  ((eq head nil)
 	   (when tail
-	     (dimwiki-justext tail)))
+	     (dimwiki--justext tail)))
 	  ((listp head)
-	   (cons (dimwiki-justext head)
-		 (dimwiki-justext tail)))
+	   (cons (dimwiki--justext head)
+		 (dimwiki--justext tail)))
 	  ((memq head '(tmpl tmpl? ref))
 	   nil)
 	  ((memq head '(link link?))
-	   (unless (dimwiki-skiplink tail)
-	     (dimwiki-justext tail)))
+	   (unless (dimwiki--skiplink tail)
+	     (dimwiki--justext tail)))
 	  ((stringp head)
-	   (cons head (dimwiki-justext tail)))
+	   (cons head (dimwiki--justext tail)))
 	  (t parsed))))
 
-(defun dimwiki-flatten (lol)
+(defun dimwiki--flatten (lol)
   (when lol
     (if (atom lol)
 	(list lol)
-      (append (dimwiki-flatten (car lol))
-	      (dimwiki-flatten (cdr lol))))))
+      (append (dimwiki--flatten (car lol))
+	      (dimwiki--flatten (cdr lol))))))
 
 
 
-(defun dimwiki-maybe-redirect (parsed)
+(defun dimwiki--maybe-redirect (parsed)
   "Return nil if no redirect, else new search term."
   (when (and (stringp (cadr parsed))
 	     (member (upcase (replace-regexp-in-string " *$" "" (cadr parsed)))
@@ -300,36 +266,64 @@ https://en.wikipedia.org/wiki/Help:Redirect has links to more languages")
 	     (stringp (cadr (cadr (cdr parsed)))))
     (cadr (cadr (cdr parsed)))))
 
-(defun dimwiki-justext-of-parsed (parsed)
+(defun dimwiki--justext-of-parsed (parsed)
   (replace-regexp-in-string
    "^\\s +" ""
    (replace-regexp-in-string
     "<!--[^>]*-->" " "
     (replace-regexp-in-string
      " *\n *\\|  +" " "
-     (apply 'concat (dimwiki-flatten
-		     (dimwiki-justext parsed)))))))
+     (apply 'concat (dimwiki--flatten
+		     (dimwiki--justext parsed)))))))
 
-(defun dimwiki-scatter-search-query (term)
+(defun dimwiki--scatter-search-query (term)
   (let ((domains dimwiki-wikidomains)
 	parsed article)
     (while (and domains (not parsed))
-      (setq article (dimwiki-extract-article-from-json
-		     (dimwiki-send-search-query term
+      (setq article (dimwiki--extract-article-from-json
+		     (dimwiki--send-search-query term
 						(car domains))))
       (if article
-	  (progn (setq parsed (dimwiki-parse article))
-		 (let ((redirect (dimwiki-maybe-redirect parsed)))
+	  (progn (setq parsed (dimwiki--parse article))
+		 (let ((redirect (dimwiki--maybe-redirect parsed)))
 		   (when redirect
 		     (setq term redirect
 			   parsed nil))))
 	(setq domains (cdr domains))))
     parsed))
 
+(defun dimwiki--dwim-at-point ()
+  "If there's an active selection, return that.
+Otherwise, get the symbol at point, as a string."
+  (cond ((use-region-p)
+         (buffer-substring-no-properties (region-beginning) (region-end)))
+        (t (thing-at-point 'word))))
+
+(defun dimwiki--read-from-minibuffer (prompt)
+  "Read a value from the minibuffer with PROMPT.
+If there's a string at point, offer that as a default."
+  (let* ((suggested (dimwiki--dwim-at-point))
+         (final-prompt
+          (if suggested
+              (format "%s (default %s): " prompt suggested)
+            (format "%s: " prompt)))
+         ;; Ask the user for input, but add `suggested' to the history
+         ;; so they can use M-n if they want to modify it.
+         (user-input (read-from-minibuffer
+                      final-prompt
+                      nil nil nil nil suggested)))
+    ;; Return the input provided by the user, or use `suggested' if
+    ;; the input was empty.
+    (if (> (length user-input) 0)
+        user-input
+      suggested)))
+
 (defun dimwiki-search (term)
-  (interactive "sSearch term: ")
-  (let ((results (dimwiki-justext-of-parsed
-		  (dimwiki-scatter-search-query term))))
+  "Look up TERM in a wiki.
+See `dimwiki-wikidomains' for what wiki's are searched."
+  (interactive (list (dimwiki--read-from-minibuffer "Search string")))
+  (let ((results (dimwiki--justext-of-parsed
+		  (dimwiki--scatter-search-query term))))
     (if (and results (not (equal "" results)))
 	(message "%s" results)
       (message "No hits!"))
